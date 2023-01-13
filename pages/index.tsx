1b0e6f2071537493e3618b0d19a7afbfb987f578
Iswar <rick731202@gmail.com>
2023-01-13 11:19:41 +0530

    Nextjs project created with tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-red-500">Heading</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
